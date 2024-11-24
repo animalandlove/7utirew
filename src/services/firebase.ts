2484edd124ac2484c45767aff902549f24484e33
animalandlove <heyveterinarian@proton.me>
2024-11-24 00:30:15 -0700

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8xi6Wam4Xj1jym2ELb7RKNfoCkWLp-WA",
-  authDomain: "animal-id-63976.firebaseapp.com",
-  projectId: "animal-id-63976",
-  storageBucket: "animal-id-63976.firebasestorage.app",
-  messagingSenderId: "567919691760",
-  appId: "1:567919691760:web:67d22aa98f326f4967ff35",
-  measurementId: "G-WQB8Y7SLRL"
+  apiKey: "AIzaSyA8xi6Wam4Xj1jym2Exxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "animal-id-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.firebaseapp.com",
+  projectId: "animal-id-6397xxxxxxxxxx",
+  storageBucket: "animal-id-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.firebasestorage.app",
+  messagingSenderId: "567919691xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx0",
+  appId: "1:5xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx691760:web:67xxxxxxxxxxxxxxxxxxxxxxxxxxx8f326f4967ff35",
+  measurementId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxB8Y7SLRL"
 };
 
 export const app = initializeApp(firebaseConfig);
